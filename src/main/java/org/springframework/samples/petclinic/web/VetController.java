d6c608aacc485ebb7fc6d0511ef23e3c5a137de4
gonguelun <gonguelun@alum.us.es>
2020-03-05 18:16:50 +0100

    Task 018-B: finished

--- src/main/java/org/springframework/samples/petclinic/web/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/web/VetController.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,16 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.samples.petclinic.web;
+
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Map;
 
 /**
  * @author Juergen Hoeller
@@ -35,13 +37,16 @@ public class VetController {
 
 	private final ClinicService clinicService;
 
+
 	@Autowired
-	public VetController(ClinicService clinicService) {
+	public VetController(final ClinicService clinicService) {
 		this.clinicService = clinicService;
 	}
 
-	@GetMapping(value = { "/vets" })
-	public String showVetList(Map<String, Object> model) {
+	@GetMapping(value = {
+		"/vets"
+	})
+	public String showVetList(final Map<String, Object> model) {
 		// Here we are returning an object of type 'Vets' rather than a collection of Vet
 		// objects
 		// so it is simpler for Object-Xml mapping
@@ -51,7 +56,9 @@ public class VetController {
 		return "vets/vetList";
 	}
 
-	@GetMapping(value = { "/vets.xml"})
+	@GetMapping(value = {
+		"/vets.xml"
+	})
 	public @ResponseBody Vets showResourcesVetList() {
 		// Here we are returning an object of type 'Vets' rather than a collection of Vet
 		// objects
@@ -59,6 +66,14 @@ public class VetController {
 		Vets vets = new Vets();
 		vets.getVetList().addAll(this.clinicService.findVets());
 		return vets;
+	}
+
+	@GetMapping(path = "/vets/{vetId}/delete")
+	public String deleteVet(@PathVariable("vetId") final int vetId) {
+		this.clinicService.deleteVetById(vetId);
+
+		return "redirect:/vets";
+
 	}
 
 }
